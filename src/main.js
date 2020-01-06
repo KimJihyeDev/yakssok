00bc5ab89db2510965f61c0ae63a5511a1884ab9
kim.jihye.dev <kim.jihye.dev@gamil.com>
2020-01-06 14:01:09 +0900

    웹팩 설정 완료

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Routes from './router/routes.js'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes:Routes
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
