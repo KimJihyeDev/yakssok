6e465187a1f07f730ab5df3b5a86b18a9c2525b5
kim.jihye.dev <kim.jihye.dev@gmail.com>
2020-01-20 20:51:26 +0900

    제품 상세 페이지 제작중

--- src/components/products/ProductDetail.vue
+++ src/components/products/ProductDetail.vue
@@ -1,18 +1,21 @@
 <template>
-    
+    <div>
+       <div style="margin:70px;">
+            <p style="top:0px; margin:0px;"> categories  >  영양제 > 비타민 </p>
+        </div> 
     <div class="site-section mt-5">
       <div class="container">
         <div class="row">
           <div class="col-lg-6">
-            <div class="owl-carousel hero-slide owl-style">
-              <img src="images/wine_1.png" alt="Image" class="img-fluid">
-              <img src="images/wine_2.png" alt="Image" class="img-fluid">
-              <img src="images/wine_3.png" alt="Image" class="img-fluid">
+            <div class="owl-style" style="text-align:center">
+              <img src="/assets/images/items/vitamin_a.png" alt="Image" class="img-fluid" style="width:60%;height:60%;">
+              <!-- <img src="/assets/images/wine_2.png" alt="Image" class="img-fluid"> -->
+              <!-- <img src="images/wine_3.png" alt="Image" class="img-fluid"> -->
             </div>
           </div>
           <div class="col-lg-5 ml-auto">
-            <h2 class="text-primary">Wine Details</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non eos inventore aspernatur voluptatibus ratione odit molestias molestiae, illum et impedit veniam modi sunt quas nam mollitia earum perferendis, dolorem. Magni.</p>
+            <h2 class="text-primary">Now Foods, Super Omega 3-6-9, 1200 mg, 180 Softgel</h2>
+            <p>제조사: Now Foods</p>
 
             <div class="mb-5">
               <div class="input-group mb-3" style="max-width: 200px;">
@@ -28,11 +31,30 @@
     
             </div>
             <p><a href="cart.html" class="buy-now btn btn-sm height-auto px-4 py-3 btn-primary">Add To Cart</a></p>
+          </div>
+        </div>
+        <!-- 추가 -->
+        <hr>
+        <div style="border:solid black 1px; height:700px;">
+          <strong style="text-align:center">제품 상세</strong>
+          <hr>
+          <div class="col-lg-5">Description
+Cardiovascular Health
+From Borage, Flax Seed & Fish Oils
+Non-GMO
+Kosher - Contains Gelatin
+A Dietary Supplement
+Nutritional Oils
+Family Owned Since 1968
+GMP Quality Assured
+Super Omega 3-6-9 is a blend of Fish, Borage and Flax Seed Oils. This combination of well-known nutritional oils has a unique balance of Omega-3 and Omega-6 Essential Fatty Acids plus Omega-9, a non-essential, but useful fatty acid. The Omega-3's, Alpha Linolenic Acid, EPA and DHA, and the Omega-6, GLA, are necessary for the maintenance of healthy skin, normal immune system balance, and proper nervous system function.
 
 
           </div>
-        </div>
+          <!-- 정보에 따라 div 나누기. sugested user, warnings ... -->
+        </div> 
+        
       </div>
     </div>
-
+    </div>
 </template>
