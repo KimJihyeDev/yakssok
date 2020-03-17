08fcf726c20bdbbdd522121fd383ef09f7081135
kim.jihye.dev <kim.jihye.dev@gmail.com>
2020-03-18 00:07:36 +0900

    화면 조정

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,62 +1,48 @@
 <template>
     <div class="site-section">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="col-md-12">
-                    <div class="col-md-12 text-center">
-                        <h2 class="h3 mb-3 text-black font-heading-serif text-black font-weight-bold">기술 스펙</h2>
-                        <div class="col-md-10 p-3 p-lg-5 border">
-
-                            <table class="table con-log-5">
-                                <tr class="teble-color">
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Front-End</strong></td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Vue.js(Vue CLI
-                                            4)</strong></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Back-End</strong></td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Node.js(Express
-                                            Framework)</strong></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>DB</strong></td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>MySQL(Sequelize
-                                            ORM)</strong></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Cloud</strong></td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Azure</strong></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>참고사이트(제품정보)</strong>
-                                    </td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>NOW Foods,
-                                            iHerb</strong></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>참고사이트(리뷰, 댓글)</strong>
-                                    </td>
-                                    <td>&nbsp;</td>
-                                    <td class="text-black font-weight-bold" colspan="2"><strong>Daum(kakao)</strong>
-                                    </td>
-                                </tr>
-                            </table>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="col-lg-6 mx-auto p-4 border">
+            <form class="px-4 py-3">
+                <table class="table con-log-5 text-center">
+                    <tr class="teble-color">
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Front-End</strong></td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Vue.js(Vue CLI
+                                4)</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Back-End</strong></td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Node.js(Express
+                                Framework)</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>DB</strong></td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>MySQL(Sequelize
+                                ORM)</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Cloud</strong></td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Azure</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>참고사이트(제품정보)</strong>
+                        </td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>NOW Foods,
+                                iHerb</strong></td>
+                    </tr>
+                    <tr>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>참고사이트(리뷰, 댓글)</strong>
+                        </td>
+                        <td>&nbsp;</td>
+                        <td class="text-black font-weight-bold" colspan="2"><strong>Daum(kakao)</strong>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+            <div class="dropdown-divider"></div>
         </div>
     </div>
 </template>
-
-<style>
-    .pointer {
-        cursor: pointer;
-    }
-</style>
